471cd06ca5b8e7cc226f7bb47f068d2a2fbc94e4
Robert de Bock <robert@meinit.nl>
2021-11-01 17:20:08 +0100

    Correct output.

--- examples/default/output.tf
+++ examples/default/output.tf
@@ -1,6 +1,6 @@
 output "consul_url" {
   description = "The URL to this consul installation."
-  value       = "http://${module.consul.aws_lb_dns_name}:8200/ui"
+  value       = "http://${module.consul.aws_lb_dns_name}:8500/ui/"
 }
 
 output "bastion_host_ip" {
