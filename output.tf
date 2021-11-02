889d841417fab6a921d296607fa3930bdb9b172e
Robert de Bock <robert@meinit.nl>
2021-11-02 14:29:20 +0100

    Fix output, showing the optional bastion host.

--- output.tf
+++ output.tf
@@ -5,7 +5,7 @@ output "aws_lb_dns_name" {
 
 output "bastion_host_public_ip" {
   description = "The IP address of the bastion host."
-  value       = try("aws_instance.bastion.public_ip", "No bastion host created.")
+  value       = try(aws_instance.bastion[0].public_ip, "No bastion host created.")
 }
 
 output "consul_instances" {
