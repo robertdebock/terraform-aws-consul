1d6254195c407dbb3966007363e811e86f4fe79a
Robert de Bock <robert@meinit.nl>
2021-11-08 16:56:00 +0100

    Tried to upgade consul 1.9.0 to latest: works.

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,8 +1,9 @@
 # Call the module.
 module "consul" {
-  source       = "../../"
-  size         = "development"
-  amount       = "3"
+  source         = "../../"
+  size           = "development"
+  amount         = "3"
+  # consul_version = "1.9.0"
   # vpc_id       = "vpc-04c93b3fa696a2d21"
   # bastion_host = false
   tags = {
